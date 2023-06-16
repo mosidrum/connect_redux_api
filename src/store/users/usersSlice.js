b783407e06a723d8e0971d7308b7716541160125
mosidrum <mosiokanga@gmail.com>
2023-06-16 04:08:52 +0100

    Redux toolkit set

---
+++ src/store/users/usersSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  users: [],
+  isLoading: false,
+  error: undefined,
+}
+
+export const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers: {}
+});
+
+export const usersReducers = usersSlice.reducer;
