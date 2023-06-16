c14c784f8999a3ece269c898a70089b84a7adc64
mosidrum <mosiokanga@gmail.com>
2023-06-16 04:18:02 +0100

    Create thunk

--- src/store/users/usersSlice.js
+++ src/store/users/usersSlice.js
@@ -1,10 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const baserUrl = 'https://randomuser.me/api/?results=5';
 
 const initialState = {
   users: [],
   isLoading: false,
   error: undefined,
 }
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  try {
+    const result = await fetch(baserUrl);
+    console.log(result);
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    throw Error('Failed to fetch users.');
+  }
+});
 
 export const usersSlice = createSlice({
   name: 'users',
